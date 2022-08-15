c0b24c87f8ddc24645c07cb295bbeedcf37c5378
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-08-15 16:28:48 -0300

    feat: implementa o retorno de realtórios simples e completos para arquivos json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -6,8 +7,25 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory():
     @classmethod
     def import_data(cls, path, report):
+        if path.endswith("csv"):
+            return cls.csv_reports(path, report)
+
+        if path.endswith("json"):
+            return cls.json_reports(path, report)
+
+    @classmethod
+    def csv_reports(cls, path, report):
         with open(path) as file:
             file_reader = list(csv.DictReader(file))
+            if report == "simples":
+                return SimpleReport.generate(file_reader)
+            if report == "completo":
+                return CompleteReport.generate(file_reader)
+
+    @classmethod
+    def json_reports(cls, path, report):
+        with open(path) as file:
+            file_reader = json.loads(file.read())
             if report == "simples":
                 return SimpleReport.generate(file_reader)
             if report == "completo":
