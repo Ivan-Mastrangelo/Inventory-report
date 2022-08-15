a290b71eae58e190716cbae617e865756f566608
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-08-15 17:05:55 -0300

    feat: implementa o retorno de realtórios simples e completos para arquivos xml na classe inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+from xml.etree import ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -12,6 +13,9 @@ class Inventory():
 
         if path.endswith("json"):
             return cls.json_reports(path, report)
+
+        if path.endswith("xml"):
+            return cls.xml_reports(path, report)
 
     @classmethod
     def csv_reports(cls, path, report):
@@ -30,3 +34,21 @@ class Inventory():
                 return SimpleReport.generate(file_reader)
             if report == "completo":
                 return CompleteReport.generate(file_reader)
+
+    @classmethod
+    def xml_reports(cls, path, report):
+        tree = ET.parse(path)
+        root = list(tree.getroot())
+        file_reader = []
+        info_dict = {}
+
+        for index in range(len(root)):
+            for info in root[index]:
+                info_dict[info.tag] = info.text
+            file_reader.append(info_dict)
+            info_dict = {}
+
+        if report == "simples":
+            return SimpleReport.generate(file_reader)
+        if report == "completo":
+            return CompleteReport.generate(file_reader)
